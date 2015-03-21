5f7f3b68324c52d7d2350ee77423a45bb30134f8
dataRJ <rubjim@yahoo.com>
2015-03-21 14:22:52 -0400

    initial commit of generate_Output.R
    initial commit of utility function to take the tidyData from
    run_Analysis() and create a new tidy Data set that includes mean values
    for the set of numeric features grouped by the Activity and Subjects.

---
+++ generate_Output.R
@@ -0,0 +1,8 @@
+generate_Output <- function(tidyData) {
+        
+        #Split Apply and Combine all in one shot
+        results <- ddply(tidyData,.(Activity,Subject),numcolwise(mean))
+        write.table(results,file="tidyData.txt",row.name=FALSE)
+        
+        results
+}
