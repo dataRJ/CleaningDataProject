b2b922c02a4258ebbfcc4e020d7a9f7b5bf66f46
dataRJ <rubjim@yahoo.com>
2015-03-21 14:44:25 -0400

    Delete generate_Means.R
    Code moved into run_Analysis.R to meet requirements of assignment.

--- generate_Means.R
+++
@@ -1,8 +0,0 @@
-generate_Means <- function(tidyData) {
-        
-        #Split Apply and Combine all in one shot
-        results <- ddply(tidyData,.(Activity,Subject),numcolwise(mean))
-        write.table(results,file="tidyData.txt",row.name=FALSE)
-        
-        results
-}
